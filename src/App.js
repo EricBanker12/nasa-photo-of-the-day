941539ab9861e81b9ec2a05d66faa34a5b31d03b
ericbanker12 <ericbanker12@gmail.com>
2019-09-12 15:14:12 -0500

    restructure components and add styled container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {useState, useEffect} from "react"
 import axios from 'axios'
+import styled from 'styled-components'
 import Header from './components/Header'
 import Image from './components/Image'
 import Info from './components/Info'
 import Input from './components/Input'
 import "./App.css"
+
+const Container = styled.div`
+    min-height: 100vh;
+    background-color: #282c34;
+    color: white;
+`
 
 function App() {
 
@@ -14,37 +21,39 @@ function App() {
 
   useEffect(()=>{
     // commented out to prevent API over-usage
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${date?`&date=${date}`:''}`)
-        .then(resp=>{
-            //console.log(resp)
-            setData(resp.data)
-        })
-        .catch(err=>{
-            console.error(err)
-        })
+    // axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${date?`&date=${date}`:''}`)
+    //     .then(resp=>{
+    //         //console.log(resp)
+    //         setData(resp.data)
+    //     })
+    //     .catch(err=>{
+    //         console.error(err)
+    //     })
 
     // temporary manual response
-    // setData({
-    //     copyright: "Bray Falls",
-    //     date: "2019-09-11",
-    //     explanation: "What energizes the Heart Nebula? First, the large emission nebula dubbed IC 1805 looks, in whole, like a human heart.  The nebula glows brightly in red light emitted by its most prominent element: hydrogen.  The red glow and the larger shape are all powered by a small group of stars near the nebula's center.  In the center of the Heart Nebula are young stars from the open star cluster Melotte 15 that are eroding away several picturesque dust pillars with their energetic light and winds. The open cluster of stars contains a few bright stars nearly 50 times the mass of our Sun, many dim stars only a fraction of the mass of our Sun, and an absent microquasar that was expelled millions of years ago.  The Heart Nebula is located about 7,500 light years away toward the constellation of Cassiopeia.  Coincidentally, a small meteor was captured in the foreground during imaging  and is visible above the dust pillars.  At the top right is the companion Fishhead Nebula.",
-    //     hdurl: "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_2378.jpg",
-    //     media_type: "image",
-    //     service_version: "v1",
-    //     title: "IC 1805: The Heart Nebula",
-    //     url: "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg",
-    // })
+    setData({
+        copyright: "Bray Falls",
+        date: "2019-09-11",
+        explanation: "What energizes the Heart Nebula? First, the large emission nebula dubbed IC 1805 looks, in whole, like a human heart.  The nebula glows brightly in red light emitted by its most prominent element: hydrogen.  The red glow and the larger shape are all powered by a small group of stars near the nebula's center.  In the center of the Heart Nebula are young stars from the open star cluster Melotte 15 that are eroding away several picturesque dust pillars with their energetic light and winds. The open cluster of stars contains a few bright stars nearly 50 times the mass of our Sun, many dim stars only a fraction of the mass of our Sun, and an absent microquasar that was expelled millions of years ago.  The Heart Nebula is located about 7,500 light years away toward the constellation of Cassiopeia.  Coincidentally, a small meteor was captured in the foreground during imaging  and is visible above the dust pillars.  At the top right is the companion Fishhead Nebula.",
+        hdurl: "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_2378.jpg",
+        media_type: "image",
+        service_version: "v1",
+        title: "IC 1805: The Heart Nebula",
+        url: "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg",
+    })
 
   },[date])
   
   return (
     <div className="App">
-      <Header />
-      <div>
+      <Container>
+        <Header />
+        <section>
+          <Input date={data.date} handler={setDate} />
+          <Info title={data.title} copyright={data.copyright} explanation={data.explanation} />
+        </section>
         <Image src={data.url} alt={data.title} hdurl={data.hdurl} />
-        <Info title={data.title} copyright={data.copyright} explanation={data.explanation} />
-      </div>
-      <Input date={data.date} handler={setDate} />
+      </Container>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header(props) {
     return (
-        <header>
-            <h2>Astronomy Picture of the Day</h2>
-        </header>
+    <header>
+        <h2>Astronomy Picture of the Day</h2>
+    </header>
     )
 }
